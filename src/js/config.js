3d129da6fa3e6d9bcd964164745a754c8b82ca36
biancaflorescu <biancaflorescu97@yahoo.com>
2021-10-13 20:11:49 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 100;
+export const RES_PER_PAGE = 10;
+export const KEY = '20413743-a52e-4dec-a133-b2b0aab585e5';
+export const MODAL_CLOSE_SEC = 2.5;
